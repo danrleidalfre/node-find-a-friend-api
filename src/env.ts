a85109d8e8beafcde8867709abff3ae8bb292ba7
Danrlei <danrleidalfre@gmail.com>
2024-01-04 18:54:40 -0300

    configure eslint

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config'
-import {z} from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
